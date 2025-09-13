ecff62c3b070d8df37fc96c99a6fd5182782cabe
josooa <jsua68652@gmail.com>
2025-09-13 21:13:03 +0900

    0913

--- week1/1966.py
+++ week1/1966.py
@@ -1,22 +1,11 @@
-import collections
 import sys
+n = int(input())
+a = list(map(int, sys.stdin.readline().split()))
+sorta = sorted(a,reverse=False)
+p = []
+for i in range(n):
+    p.append(sorta.index(a[i]))
+    sorta[sorta.index(a[i])] = -1
 
-tc= int(input())
-for i in range(tc):
-    n, m = map(int, input().split())
-    queue = collections.deque(list(map(int, sys.stdin.readline().split())))
-    count = 0
-    p=m
-    while (queue):
-        front = queue.popleft() 
-        valuehigh = max(queue) 
-        p-=1 
-        if (valuehigh==front): 
-            count+=1
-            if (p<0): 
-                print(count)
-                break
-        else:  
-            queue.append(front) 
-            if (p<0) :  
-                p=len(queue) - 1
+for i in range(n):
+    print(str(p[i]),end=' ')
